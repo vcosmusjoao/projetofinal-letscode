3f0f396037a048c7b67b0a2e300e3c9175d75051
vcosmusjoao <rm88372@fiap.com.br>
2021-11-08 16:08:07 -0300

    Adcionando classes e adcionando propriedades do Banco h2.

---
+++ src/main/java/br/com/letscode/spring/projetofinal/controller/AnotacaoController.java
@@ -0,0 +1,4 @@
+package br.com.letscode.spring.projetofinal.controller;
+
+public class AnotacaoController {
+}
---
+++ src/main/java/br/com/letscode/spring/projetofinal/repository/UsuarioRepository.java
@@ -0,0 +1,4 @@
+package br.com.letscode.spring.projetofinal.repository;
+
+public interface UsuarioRepository {
+}
