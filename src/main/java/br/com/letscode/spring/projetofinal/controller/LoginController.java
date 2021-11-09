31c8035927a3163c68f2a76bb4cf3f9e79cfb63b
vcosmusjoao <rm88372@fiap.com.br>
2021-11-09 20:04:06 -0300

    Adcionando templates em HTML.

---
+++ src/main/java/br/com/letscode/spring/projetofinal/controller/LoginController.java
@@ -0,0 +1,51 @@
+package br.com.letscode.spring.projetofinal.controller;
+
+import br.com.letscode.spring.projetofinal.model.LoginForm;
+import br.com.letscode.spring.projetofinal.model.Usuario;
+import br.com.letscode.spring.projetofinal.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    private UsuarioRepository userRepo;
+
+    @GetMapping("/registra")
+    public String mostrarFormularioDeRegistro(Model model) {
+        model.addAttribute("usuario", new Usuario());
+        return "cadastro";
+    }
+
+    @PostMapping("/processo_registrar")
+    public String processRegister(Usuario usuario) {
+        userRepo.save(usuario);
+
+        return "login";
+    }
+
+
+    @RequestMapping( value="/login", method = RequestMethod.GET)
+    public String getLoginForm(){
+        return "login";
+    }
+
+    @RequestMapping( value="/login", method = RequestMethod.POST)
+    public String login(@ModelAttribute(name="loginform")LoginForm loginForm, Model model){
+        String email=loginForm.getEmail();
+        String senha= loginForm.getSenha();
+
+        Usuario usuario=userRepo.findByEmail(email);
+
+        model.addAttribute(usuario.getNome(),true);
+
+        return "login";
+    }
+
+
+
+
+}
