5f2f3c28854c1ec8f9f99953db38da954f65f270
vcosmusjoao <rm88372@fiap.com.br>
2021-11-09 00:22:47 -0300

    Adcionando cadastro de usuarios/login.

--- src/main/java/br/com/letscode/spring/projetofinal/model/Usuario.java
+++ src/main/java/br/com/letscode/spring/projetofinal/model/Usuario.java
@@ -1,4 +1,27 @@
 package br.com.letscode.spring.projetofinal.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false, unique = true, length = 45)
+    private String email;
+    @Column(nullable = false)
+    private String senha;
+
 }
+
--- src/main/java/br/com/letscode/spring/projetofinal/repository/UsuarioRepository.java
+++ src/main/java/br/com/letscode/spring/projetofinal/repository/UsuarioRepository.java
@@ -1,4 +1,12 @@
 package br.com.letscode.spring.projetofinal.repository;
 
-public interface UsuarioRepository {
+import br.com.letscode.spring.projetofinal.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    @Query("SELECT u FROM Usuario u WHERE u.email = ?1")
+    public Usuario findByEmail(String email);
+
+
 }
