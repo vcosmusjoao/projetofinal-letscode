3365f8afd96949ad0b3cbf559dd94219fc37cfcb
Lauredi Gustavo <68924576+LaurediGustavo@users.noreply.github.com>
2021-11-11 17:35:23 -0300

    exibit anotacoes

--- src/main/java/br/com/letscode/spring/projetofinal/controller/AnotacaoController.java
+++ src/main/java/br/com/letscode/spring/projetofinal/controller/AnotacaoController.java
@@ -5,9 +5,8 @@ import br.com.letscode.spring.projetofinal.model.UsuariosLogados;
 import br.com.letscode.spring.projetofinal.repository.AnotacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AnotacaoController {
@@ -27,6 +26,15 @@ public class AnotacaoController {
     public String processRegister(Anotacao anotacao) {
         anotacao.setUsuario(usuariosLogados.getUsuarios());
         anotacaoRepository.save(anotacao);
+
         return "redirect:/home";
+    }
+
+    @GetMapping("/anotacao/{id}")
+    public String getAnotacao(@PathVariable Long id, Model model) {
+        Anotacao anotacao = anotacaoRepository.getById(id);
+        model.addAttribute("anotacao", anotacao);
+
+        return "/anotacao";
     }
 }
